6955586ce395571ce48910e3839ddf89a8710e52
henrybissonnette <henrydbissonnette@gmail.com>
2020-02-26 22:22:31 -0500

    questionaire

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const questions = [{
+    q: "Who are you?",
+    rs: [
+      "A person.",
+      "Nobody.",
+      "Just me."
+    ]
+  },
+  {
+    q: "What's going on?",
+    rs:[
+      "Nothing.",
+      "Something.",
+      "All the things."
+    ]
+  }
+]
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +28,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Questionaire questions={questions} />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +40,102 @@ function App() {
       </header>
     </div>
   );
+}
+
+class Questionaire extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      results: {}
+    }
+  }
+
+  registerResponse(q, a) {
+    var results = {...this.state.results}
+    results[q] = a
+    this.setState({
+      results: results
+    })
+  }
+
+  displayQuestions() {
+    return this.props.questions.map((q) =>
+      <TextOptionSelect
+        question={q.q}
+        responses={q.rs}
+        registerResponse={(q, a) => this.registerResponse(q, a)}
+      />
+    )
+  }
+
+  hasResults() {
+    const r = this.state.results
+    return Object.entries(r).length !== 0 && r.constructor === Object
+  }
+
+  displayResultSummary() {
+    return Object.keys(this.state.results).map((question) => 
+      <p>
+        Q: {question} A: {this.state.results[question]}
+      </p>
+    )
+  }
+
+  displayResults() {
+    if (this.hasResults()) {
+      return <div>
+          <h2>Questionaire Results</h2>
+          {this.displayResultSummary()}
+        </div>
+    }
+  }
+
+  render() {
+    return <div>
+        <h2>Please answer the following questions.</h2>
+        {this.displayQuestions()}
+        {this.displayResults()}
+      </div>
+  }
+}
+
+class TextOptionSelect extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  onSelect(question, response) {
+    this.setState({activeResponse: response})
+    this.props.registerResponse(question, response)
+  }
+
+  activityClass(response) {
+    if ("activeResponse" in this.state && this.state.activeResponse === response) {
+      return "active"
+    }
+  }
+
+  render() {
+    return <div className="text-option-select">
+      <p>
+        Q: {this.props.question}
+      </p>
+      
+      <p>
+        <ul>
+          {this.props.responses.map((r) =>
+            <li 
+              onClick={() => this.onSelect(this.props.question, r)}
+              className={this.activityClass(r)}
+            >
+              {r}
+            </li>
+          )}
+        </ul>
+      </p>
+    </div>
+  }
 }
 
 export default App;
